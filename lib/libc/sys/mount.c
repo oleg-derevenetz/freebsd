b1d873ac11e611762b3ce2319ea860c6e4e1a782
Oleg Derevenetz <oleg.derevenetz@gmail.com>
2019-06-13 01:39:35 +0300

    Remove "All rights reserved" clause from copyright.

--- lib/libc/sys/mount.c
+++ lib/libc/sys/mount.c
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2019 Oleg Derevenetz <oleg.derevenetz@gmail.com>
- * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
